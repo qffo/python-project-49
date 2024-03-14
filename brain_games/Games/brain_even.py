3c0549e991257a4826f57d334719c8fc9d2e06fe
qffo <075viktor@mail.ru>
2024-03-14 11:34:42 +0300

    modif code

--- brain_games/Games/brain_even.py
+++ brain_games/Games/brain_even.py
@@ -5,35 +5,33 @@ import prompt
 from ..cli import welcome_user
 
 
-# def game_info():
-#     print('Answer "yes" if the number is even, otherwise answer "no".')
-
-
 def wrong_answer_message(usr_ans, ans, name):
     return print(f"""'{usr_ans}' is wrong answer ;(. Correct answer was '{ans}'.
 Let's try again, {name}!""")
+
+
+def foo():
+    pass
 
 
 def even_game():
     name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
     i = 0
-    while i < 3 and name:
+    while i < 3:
         number = randint(1, 10)
         print(f'Question: {number}')
         user_answer = prompt.string('Your answer: ')
-        if number % 2 == 0:
-            if number % 2 == 0 and user_answer == 'yes':
-                print('Correct!')
-            else:
-                wrong_answer_message(user_answer, 'yes', name)
-                break
-        if number % 2 != 0:
-            if number % 2 != 0 and user_answer == 'no':
-                print('Correct!')
-            else:
-                wrong_answer_message(user_answer, 'no', name)
-                break
+        if number % 2 == 0 and user_answer == 'yes':
+            print('Correct!')
+        if number % 2 == 0 and user_answer == 'no':
+            wrong_answer_message(user_answer, 'yes', name)
+            break
+        if number % 2 != 0 and user_answer == 'no':
+            print('Correct!')
+        if number % 2 != 0 and user_answer == 'yes':
+            wrong_answer_message(user_answer, 'no', name)
+            break
         i += 1
     if i == 3:
         print(f"Congratulations, {name}!")
