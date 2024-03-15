2930c42424da8acd2e08103e0509b26269b92887
qffo <075viktor@mail.ru>
2024-03-15 10:09:59 +0300

    add new file brain_calc

---
+++ brain_games/games/brain_calc.py
@@ -0,0 +1,29 @@
+import prompt
+
+from ..cli import welcome_user
+from ..game_logic import random_number, wrong_answer, welcome, win_game
+
+
+def calc_game():
+    welcome()
+    name = welcome_user()
+    print('What is the result of the expression?')
+    i = 0
+    while i < 3:
+        game_process(name)
+        i += 1
+    if i == 3:
+        win_game(name)
+
+
+def game_process(name):
+    name = name
+    num_one = random_number()
+    num_two = random_number()
+    answer = num_one + num_two
+    print(f'Question: {num_one} + {num_two}')
+    user_answer = prompt.string('Your answer: ')
+    if user_answer == str(answer):
+        print('Correct!')
+    else:
+        wrong_answer(user_answer, answer, name)
