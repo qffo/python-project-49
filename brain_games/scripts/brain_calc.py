2f9bba9686ccfc0ca69d553c5e03e590929e3a74
qffo <075viktor@mail.ru>
2024-03-15 10:11:34 +0300

    add new script brain_calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+from ..games.brain_calc import calc_game
+
 
 def main():
-    print("Welcome to the Brain Games!")
+    # print("Welcome to the Brain Games!")
+    calc_game()
 
 
 if __name__ == '__main__':
